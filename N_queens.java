1ccefa0b14137b200e92ec383c3cbde421a4da77
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-10-15 13:07:46 +0500

    N x N cheess

--- N_queens.java
+++ N_queens.java
@@ -39,7 +39,7 @@ for (int c = col; c>=0 && r>=0; c--,r--) {
 }
 //upper right
 r=row;
-for (int c = col; col < board.length && r>=0; r--,c++) {
+for (int c = col; c < board.length && r>=0; r--,c++) {
     if (board[r][c]=='Q') {
         return false;
         
@@ -49,7 +49,7 @@ for (int c = col; col < board.length && r>=0; r--,c++) {
 
 //lower left 
 r=row;
-for (int c = col; col>=0 && r<board.length; r++,c--) {
+for (int c = col; c>=0 && r<board.length; r++,c--) {
     if (board[r][c]=='Q') {
         return false;
         
@@ -57,16 +57,35 @@ for (int c = col; col>=0 && r<board.length; r++,c--) {
     
 }
 //lower right
-for (int c = col; col<board.length && r<board.length; r++,c++) {
+for (int c = col; c<board.length && r<board.length; r++,c++) {
     if (board[r][c]=='Q') {
         return false;
         
     }
     
 }
+}
 
 
+public void saveBoard(char[][] board, List<List<String>> allBoards){
 
+    String row="";
+    List<String> newBoard = new ArrayList<>();
+    for (int i = 0; i < board.length; i++) {
+        row="";
+        for (int j = 0; j < board[0].length; j++) {
+            if (board[i][j]=='Q') {
+                row+="Q";
+               
+
+                
+            } else{
+                    row+='.';
+                }
+                newBoard.add(row);
+        }
+    }
+    allBoards.add(newBoard);
 
 }
 
@@ -96,7 +115,7 @@ if (isSafe(row,col,board)) {
        char[][] board = new board[n][n];
 
        helper(board, allBoards, 0);
-
+return allBoards;
 }
 
 
