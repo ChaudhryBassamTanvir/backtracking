abe9739673da1ee06a54cae92dc9e38e626057c3
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-10-15 13:01:04 +0500

    N x N cheess

--- N_queens.java
+++ N_queens.java
@@ -15,7 +15,59 @@ for(int j=0;j<board.length;j++ ){
        return false;
         
     }
+
+
+
 }
+// verticle  
+for (int i = 0; i < board[0].length; i++) {
+ if (board[i][col]=='Q') {
+    return false;
+
+    
+ }
+    
+}
+//upper left 
+int r=row;
+for (int c = col; c>=0 && r>=0; c--,r--) {
+    if (board[r][c]=='Q') {
+        return false;
+        
+    }
+    
+}
+//upper right
+r=row;
+for (int c = col; col < board.length && r>=0; r--,c++) {
+    if (board[r][c]=='Q') {
+        return false;
+        
+    }
+    
+}
+
+//lower left 
+r=row;
+for (int c = col; col>=0 && r<board.length; r++,c--) {
+    if (board[r][c]=='Q') {
+        return false;
+        
+    }
+    
+}
+//lower right
+for (int c = col; col<board.length && r<board.length; r++,c++) {
+    if (board[r][c]=='Q') {
+        return false;
+        
+    }
+    
+}
+
+
+
+
 }
 
 
